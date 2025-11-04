c56d18d8bfa6ac10a18d7d67b182330e5a8adfd1
k.pradeep <hellopradeep69@gmail.com>
2025-11-04 22:25:49 +0530

    Drawing pot with border

--- main.c
+++ main.c
@@ -1,97 +1,110 @@
 #include <ncurses.h>
 #include <stdbool.h>
-#include <string.h>
-
-#define MAX_POINTS 10000 // simple fixed-size canvas (for demo)
-
-typedef struct {
-  int y, x;
-} Point;
 
 int main() {
   initscr();
   noecho();
-  curs_set(0);
+  curs_set(2);
   keypad(stdscr, TRUE);
 
   int max_y, max_x;
   getmaxyx(stdscr, max_y, max_x);
 
   int y = max_y / 2, x = max_x / 2;
-  bool drawing = false, erasing = false;
+  bool drawing = false;
+  bool erasing = false;
 
-  Point canvas[MAX_POINTS];
-  int canvas_size = 0;
+  border(0, 0, 0, 0, 0, 0, 0, 0);
+  mvprintw(0, 2, "Drawpot");
+  mvprintw(max_y - 1, 1,
+           " hjkl = move | w/e/b = jump (draws if active) | SPACE = Toggle "
+           "draw | x = "
+           "erase | q = quit ");
+  move(y, x);
+  refresh();
 
-  const char *title = "Drawpit";
+  int ch;
+  while ((ch = getch()) != 'q') {
+    int steps = 0; // for drawing multiple points
+    int dx = 0, dy = 0;
 
-  while (1) {
-    clear();
-
-    // Draw title centered
-    mvprintw(0, (max_x - (int)strlen(title)) / 2, "%s", title);
-
-    // Draw all points
-    for (int i = 0; i < canvas_size; i++) {
-      mvaddch(canvas[i].y, canvas[i].x, '*');
-    }
-
-    // Choose cursor char
-    char cursor_char = '@';
-    if (drawing)
-      cursor_char = '#';
-    else if (erasing)
-      cursor_char = 'X';
-
-    mvaddch(y, x, cursor_char);
-    refresh();
-
-    int ch = getch();
-
-    if (ch == 'q')
-      break;              // Quit
-    else if (ch == ' ') { // Toggle drawing
+    switch (ch) {
+    // Basic movement
+    case KEY_LEFT:
+    case 'h':
+      dx = -1;
+      steps = 2;
+      break;
+    case KEY_RIGHT:
+    case 'l':
+      dx = 1;
+      steps = 2;
+      break;
+    case KEY_UP:
+    case 'k':
+      dy = -1;
+      steps = 2;
+      break;
+    case KEY_DOWN:
+    case 'j':
+      dy = 1;
+      steps = 2;
+      break;
+    case 'w':
+      dx = 1;
+      steps = 5;
+      break; // jump right
+    case 'b':
+      dx = -1;
+      steps = 5;
+      break; // jump left
+    case 'd':
+      dy = 1;
+      steps = 3;
+      break; // jump down
+    case 'u':
+      dy = -1;
+      steps = 3;
+      break; // jump down
+    case ' ':
       drawing = !drawing;
       erasing = false;
-      if (drawing && canvas_size < MAX_POINTS)
-        canvas[canvas_size++] = (Point){y, x};
-    } else if (ch == 'x') { // Toggle erasing
+      break;
+    case 'x':
       erasing = !erasing;
       drawing = false;
-      // remove current point
-      for (int i = 0; i < canvas_size; i++) {
-        if (canvas[i].y == y && canvas[i].x == x) {
-          for (int j = i; j < canvas_size - 1; j++)
-            canvas[j] = canvas[j + 1];
-          canvas_size--;
-          break;
-        }
-      }
-    } else if (ch == 'c') { // Clear canvas
-      canvas_size = 0;
-    } else if (ch == 'h' && x > 0)
-      x--;
-    else if (ch == 'l' && x < max_x - 1)
-      x++;
-    else if (ch == 'k' && y > 1)
-      y--;
-    else if (ch == 'j' && y < max_y - 1)
-      y++;
+      break;
+    }
 
-    // Drawing or erasing as you move
-    if (drawing && canvas_size < MAX_POINTS) {
-      canvas[canvas_size++] = (Point){y, x};
-    }
-    if (erasing) {
-      for (int i = 0; i < canvas_size; i++) {
-        if (canvas[i].y == y && canvas[i].x == x) {
-          for (int j = i; j < canvas_size - 1; j++)
-            canvas[j] = canvas[j + 1];
-          canvas_size--;
-          break;
-        }
+    // Move with multiple steps
+    for (int i = 0; i < steps; i++) {
+      x += dx;
+      y += dy;
+
+      // Boundary check
+      if (x < 1)
+        x = 1;
+      if (x > max_x - 2)
+        x = max_x - 2;
+      if (y < 1)
+        y = 1;
+      if (y > max_y - 2)
+        y = max_y - 2;
+
+      // Draw trail if active
+      if (drawing) {
+        mvaddch(y, x, '*');
+        mvprintw(0, 2, "Drawing");
+      } else if (erasing) {
+        mvaddch(y, x, ' ');
+        mvprintw(0, 2, "Erasing");
+      } else {
+        mvprintw(0, 2, "Drawpot");
       }
     }
+
+    move(y, x);
+    refresh();
   }
 
   endwin();
